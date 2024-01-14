df93498ea503510b2b84822de7db610f84886931
moonstar-x <christian.lopez99@outlook.com>
2024-01-13 19:07:16 -0500

    Added paths support for storybook.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,6 @@
 import type { StorybookConfig } from '@storybook/nextjs';
+import path from 'path';
+import tsConfig from '../tsconfig.json';
 
 const config: StorybookConfig = {
   stories: ['../src/components/**/*.mdx', '../src/components/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -14,6 +16,22 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: 'tag'
+  },
+  webpackFinal: (config) => {
+    const aliases = tsConfig.compilerOptions.paths;
+
+    Object.entries(aliases).forEach(([alias, [value]]) => {
+      const matchedAlias = /^(@\w+)/.exec(alias)?.groups?.[1];
+      const matchedPath = value.replace(/\/\*$/, '');
+
+      if (matchedAlias) {
+        if (config.resolve?.alias) {
+          (config.resolve.alias as any)[matchedAlias] = path.resolve(__dirname, '..', matchedPath);
+        }
+      }
+    });
+
+    return config;
   }
 };
 
