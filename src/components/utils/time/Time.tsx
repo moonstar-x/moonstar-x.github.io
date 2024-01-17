215c7773bd5b7eb984b069005144c85413eccb39
moonstar-x <christian.lopez99@outlook.com>
2024-01-17 02:50:55 -0500

    Updated implementation of Time component.

--- src/components/utils/time/Time.tsx
+++ src/components/utils/time/Time.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import clsx from 'clsx';
 
-export interface Props {
+export interface Props extends React.DetailedHTMLProps<React.TimeHTMLAttributes<HTMLTimeElement>, HTMLTimeElement> {
   date: Date | string
   withTime?: boolean
 }
 
-export const Time: React.FC<Props> = ({ date, withTime }) => {
+export const Time: React.FC<Props> = ({ date, withTime, className, ...props }) => {
   const dateObj: Date = typeof date === 'string' ? new Date(date) : date;
   const options: Intl.DateTimeFormatOptions = {
     year: 'numeric',
@@ -13,9 +14,13 @@ export const Time: React.FC<Props> = ({ date, withTime }) => {
     day: 'numeric'
   };
 
-  if (withTime) {
-    return dateObj.toLocaleTimeString('en-US', options);
-  }
+  const formattedTime = withTime ?
+    dateObj.toLocaleTimeString('en-US', options) :
+    dateObj.toLocaleDateString('en-US', options);
 
-  return dateObj.toLocaleDateString('en-US', options);
+  return (
+    <time className={clsx('typography-body1', className)} dateTime={dateObj.toISOString()} {...props}>
+      {formattedTime}
+    </time>
+  );
 };
