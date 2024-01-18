3f28100834a861e14ebbfef34583138cc81fdcd0
moonstar-x <christian.lopez99@outlook.com>
2024-01-17 23:17:45 -0500

    Defined work metadata.

--- src/lib/services/work.ts
+++ src/lib/services/work.ts
@@ -1,20 +1,40 @@
 import path from 'path';
-import { getAllSlugs, getAllMetadata, getContent } from '@lib/services/markdown';
+import { getAllSlugs, getAllMetadata, getContent, Markdown } from '@lib/services/markdown';
 
 const directory = path.join(process.cwd(), 'src/data/_work');
 
-export interface ProjectMetadata {
+export type WorkStatus = 'completed' | 'maintained' | 'in-development' | 'deprecated' | 'abandoned';
+export type WorkType = 'hobby' | 'art' | 'research';
+
+export type WorkLinkType = 'github' | 'dockerhub' | 'website' | 'discord' | 'npm' | 'steam';
+export type WorkStatsType = 'github' | 'dockerhub' | 'npm';
+
+export interface WorkMetadata {
   name: string
+  description: string
+  cover: string
+  data: string
+  technologies: string[]
+  status: WorkStatus
+  type: WorkType
+  links?: {
+    [k in WorkLinkType]?: string
+  }
+  stats?: {
+    [k in WorkStatsType]?: string
+  }
 }
+
+export type WorkArticle = Markdown<WorkMetadata>
 
 export const getAllWorkSlugs = () => {
   return getAllSlugs(directory);
 };
 
 export const getAllWorkMetadata = () => {
-  return getAllMetadata<ProjectMetadata>(directory);
+  return getAllMetadata<WorkMetadata>(directory);
 };
 
-export const getWorkBySlug = (slug: string) => {
-  return getContent<ProjectMetadata>(directory, slug);
+export const getWorkBySlug = (slug: string): Promise<WorkArticle> => {
+  return getContent<WorkMetadata>(directory, slug);
 };
