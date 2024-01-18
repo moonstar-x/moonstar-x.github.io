c6802d2bb40b836d35151804eeae63655cc653aa
moonstar-x <christian.lopez99@outlook.com>
2024-01-17 22:27:29 -0500

    Implemented PostsGrid.

---
+++ src/components/blog/postsGrid/NoPostsAvailable.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const NoPostsAvailable = () => {
+  return (
+    <div>
+      NO POSTS
+    </div>
+  );
+};
--- src/components/blog/postsGrid/PostsGrid.tsx
+++ src/components/blog/postsGrid/PostsGrid.tsx
@@ -1,10 +1,38 @@
 import React from 'react';
+import clsx from 'clsx';
+import { PostCard } from '@components/blog/postCard';
+import { NoPostsAvailable } from './NoPostsAvailable';
+import { BlogPost } from '@lib/services/blog';
 
-// TODO: Implement this component.
-export const PostsGrid = () => {
+
+export interface Props extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> {
+  posts: BlogPost['metadata'][]
+}
+
+export const PostsGrid: React.FC<Props> = ({ posts, className, ...props }) => {
+  if (!posts.length) {
+    return (
+      <NoPostsAvailable />
+    );
+  }
+
+  const [firstPost, ...restOfPosts] = posts;
+
   return (
-    <section>
-      PostsGrid
+    <section className={clsx('page-container', className)} {...props}>
+      <PostCard post={firstPost} featured />
+
+      {
+        restOfPosts.length > 1 && (
+          <div className="mt-[40px] tablet:mt-[80px] grid grid-cols-1 tablet:grid-cols-2 desktop:grid-cols-3 gap-x-[40px] gap-y-[40px] tablet.gap-y-[80px]">
+            {
+              restOfPosts.map((post, idx) => (
+                <PostCard key={idx} post={post} />
+              ))
+            }
+          </div>
+        )
+      }
     </section>
   );
 };
