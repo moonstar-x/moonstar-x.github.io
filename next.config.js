f0ae66e81c4d7489fe2b086dfe1f81cdda4b0fad
moonstar-x <christian.lopez99@outlook.com>
2024-01-26 01:00:08 -0500

    Added trailingSlash setting

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  trailingSlash: false,
+  trailingSlash: true,
   output: 'export',
   distDir: 'build',
   images: {
