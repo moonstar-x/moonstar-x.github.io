85c73ed5ae2429f7ac6f22a35582da04734b1bb1
moonstar-x <moonstar-x@users.noreply.github.com>
2024-01-29 00:25:08 +0000

    deploy: 8d733f0ed76a38def98aa1e135e4ac61d22126ba

--- _next/static/chunks/app/blog/page-c1aeffdb44e4e5c4.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[404,160,534],{4894:function(e,n,s){Promise.resolve().then(s.t.bind(s,1749,23)),Promise.resolve().then(s.t.bind(s,5250,23))}},function(e){e.O(0,[749,250,971,938,744],function(){return e(e.s=4894)}),_N_E=e.O()}]);
---
+++ _next/static/chunks/app/work/[slug]/page-e1c5b935c301d4ed.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[798,308],{},function(n){n.O(0,[366,250,245,971,938,744],function(){return n(n.s=7245)}),_N_E=n.O()}]);
