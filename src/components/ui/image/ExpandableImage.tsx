8d733f0ed76a38def98aa1e135e4ac61d22126ba
moonstar-x <christian.lopez99@outlook.com>
2024-01-28 19:22:52 -0500

    Added animation ExpandableImage.

--- src/components/ui/image/ExpandableImage.tsx
+++ src/components/ui/image/ExpandableImage.tsx
@@ -2,6 +2,7 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 'use client';
 import React, { Fragment, useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 import clsx from 'clsx';
 import { Image, Props as ImageProps } from './Image';
 import { Icon } from '@components/ui/icon';
@@ -42,29 +43,38 @@ export const ExpandableImage: React.FC<Props> = ({
         {...props}
       />
 
-      {
-        open && (
-          <div className="fixed top-0 right-0 bottom-0 left-0 bg-transparent-80 z-10" onClick={handleClose}>
-            <div className="w-full h-full flex flex-col gap-[1rem] px-[1rem] py-[2rem]">
-              <Icon
-                icon={XMarkIcon}
-                size="2x"
-                role="button"
-                className="self-end justify-self-center fill-white opacity-50 hover:opacity-100"
-                onClick={handleClose}
-              />
-
-              <div className="flex-1 h-0 relative">
-                <Image
-                  className={clsx('!rounded-none h-auto w-auto max-w-full max-h-full m-auto absolute top-0 right-0 bottom-0 left-0', className)}
-                  onClick={stopPropagation}
-                  {...props}
+      <AnimatePresence>
+        {
+          open && (
+            <motion.div
+              className="fixed top-0 right-0 bottom-0 left-0 bg-transparent-80 z-10"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ ease: 'easeInOut', duration: 0.2 }}
+              onClick={handleClose}
+            >
+              <div className="w-full h-full flex flex-col gap-[1rem] px-[1rem] py-[2rem]">
+                <Icon
+                  icon={XMarkIcon}
+                  size="2x"
+                  role="button"
+                  className="self-end justify-self-center fill-white opacity-50 hover:opacity-100"
+                  onClick={handleClose}
                 />
+
+                <div className="flex-1 h-0 relative">
+                  <Image
+                    className={clsx('!rounded-none h-auto w-auto max-w-full max-h-full m-auto absolute top-0 right-0 bottom-0 left-0', className)}
+                    onClick={stopPropagation}
+                    {...props}
+                  />
+                </div>
               </div>
-            </div>
-          </div>
-        )
-      }
+            </motion.div>
+          )
+        }
+      </AnimatePresence>
     </Fragment>
   );
 };
